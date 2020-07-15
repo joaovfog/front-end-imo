241beb67668fb8f109dd2d14a1d21c3e74f11f76
joaovfog <j.vitor48@outlook.com>
2020-07-15 09:58:52 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AppLayoutComponent } from './layout/app-layout/app-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AppLayoutComponent
   ],
   imports: [
     BrowserModule,
